790f900c12c035b0b14e49a816b1aafefef7c0fa
Lázaro Bodevan <lazarobodevan@gmail.com>
2025-10-06 20:22:52 -0300

    Discount rpc

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,46 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services {
+    public class DiscountService 
+        (DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase{
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context) {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if(coupon is null) {
+                coupon = new Models.Coupon { ProductName = "No Discount", Amount = 0, Description= string.Empty };
+            }
+
+            logger.LogInformation("Discount is retrieved for {productName}, {amount}", coupon.ProductName, coupon.Amount);
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context) {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request"));
+
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount created for product {productName}", coupon.ProductName);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context) {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context) {
+            return base.DeleteDiscount(request, context);
+        }
+    }
+}
