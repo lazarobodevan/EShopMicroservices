cbdc116a40fba2fec9975bc4145e21cc6006d53d
Lázaro Bodevan <lazarobodevan@gmail.com>
2025-10-21 01:24:09 -0300

    Feat(ordering): domain layer completed - rich domain model entities

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -14,5 +14,26 @@ namespace Ordering.Domain.ValueObjects {
 
         public string ZipCode { get; } = default!;
 
+        protected Address() { }
+
+        private Address(string firstName, string lastName, string emailAddress, string adressLine, string country, string state, string zipCode) {
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            AddressLine = adressLine;
+            Country = country;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        public static Address Of(string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string zipcode) {
+
+            ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+            ArgumentException.ThrowIfNullOrWhiteSpace(addressLine);
+
+            return new Address(firstName, lastName, emailAddress, addressLine, country, state, zipcode);
+
+        }
+
     }
 }
