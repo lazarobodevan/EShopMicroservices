7b637e28736c949c1cbcc7962215126b65ef604e
Lazaro Izidoro Bodevan Maia <lazaro.maia@araujo.com.br>
2026-03-18 11:45:12 -0300

    feat: update order

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -37,7 +37,7 @@ namespace Ordering.Domain.Models {
             return order;
         }
 
-        public void Update(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status) {
+        public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status) {
             OrderName = orderName;
             ShippingAddress = shippingAddress;
             BillingAddress = billingAddress;
