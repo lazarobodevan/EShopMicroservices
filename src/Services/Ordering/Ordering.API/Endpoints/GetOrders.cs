fc1fd8fb6a3093579f916dd60620388b7c874a7c
Lazaro Izidoro Bodevan Maia <lazaro.maia@araujo.com.br>
2026-03-18 14:45:16 -0300

    feat: orders api

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -0,0 +1,23 @@
+﻿
+using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
+{
+
+    public record GetOrdersResponse(IEnumerable<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+                var response = result.Adapt<GetOrdersResponse>();
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
